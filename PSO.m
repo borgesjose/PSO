3659dbeada1887f71cd81c5b2e165f3f248c2dee
borgesjose <jose.borges90@hotmail.com>
2022-06-18 23:40:33 -0300

    18-06-2022 version

--- PSO.m
+++ PSO.m
@@ -1,5 +1,11 @@
-function [GBEST,cgCurve] = PSO(noP, maxIter, pso, problem, datavis )
+function [GBEST,cgCurve] = PSO(noP, maxIter, pso, problem, dataVis  )
 
+% Computation of PSO Algorithms.
+% Inputs: noP = Numero de particulas  
+%         maxIter = Numero de interações 
+%         pso =  Parametros do PSO.
+%         problem =  Struct com os valores dos dos parametros do sistema
+%         datavis Flag para visualização dos gráficos
 
 
 nVar = problem.nVar;
@@ -21,7 +27,8 @@ FirstP_D1 = zeros(1 , maxIter);
 position_history = zeros(noP , maxIter , nVar );
 
 % The PSO algorithm
-% Initialize the particles
+%% Step1: Randomly initialize Swarm population of N particles
+
 for k = 1 : noP
     Swarm.Particles(k).X = (ub-lb) .* rand(1,nVar) + lb;
     Swarm.Particles(k).V = zeros(1, nVar);
@@ -32,6 +39,75 @@ for k = 1 : noP
     Swarm.GBEST.O = inf;
 end
 
+for t = 1 : maxIter
+    
+    % Calcualte the objective value
+    for k = 1 : noP
+        
+        currentX = Swarm.Particles(k).X;
+        position_history(k , t , : ) = currentX;
+        
+        
+        Swarm.Particles(k).O = fobj(currentX);
+        average_objective(t) =  average_objective(t)  + Swarm.Particles(k).O;
+        
+        % Update the PBEST
+        if Swarm.Particles(k).O < Swarm.Particles(k).PBEST.O
+            Swarm.Particles(k).PBEST.X = currentX;
+            Swarm.Particles(k).PBEST.O = Swarm.Particles(k).O;
+        end
+        
+        % Update the GBEST
+        if Swarm.Particles(k).O < Swarm.GBEST.O
+            Swarm.GBEST.X = currentX;
+            Swarm.GBEST.O = Swarm.Particles(k).O;
+        end
+    end
+    
+    % Update the X and V vectors
+    w = wMax - t .* ((wMax - wMin) / maxIter);
+    
+    FirstP_D1(t) = Swarm.Particles(1).X(1);
+    
+    for k = 1 : noP
+        Swarm.Particles(k).V = w .* Swarm.Particles(k).V + c1 .* rand(1,nVar) .* (Swarm.Particles(k).PBEST.X - Swarm.Particles(k).X) ...
+            + c2 .* rand(1,nVar) .* (Swarm.GBEST.X - Swarm.Particles(k).X);
+        
+        
+        % Check velocities
+        index1 = find(Swarm.Particles(k).V > vMax);
+        index2 = find(Swarm.Particles(k).V < vMin);
+        
+        Swarm.Particles(k).V(index1) = vMax(index1);
+        Swarm.Particles(k).V(index2) = vMin(index2);
+        
+        Swarm.Particles(k).X = Swarm.Particles(k).X + Swarm.Particles(k).V;
+        
+        % Check positions
+        index1 = find(Swarm.Particles(k).X > ub);
+        index2 = find(Swarm.Particles(k).X < lb);
+        
+        Swarm.Particles(k).X(index1) = ub(index1);
+        Swarm.Particles(k).X(index2) = lb(index2);
+        
+    end
+    
+    if dataVis == 1
+        outmsg = ['Iteration# ', num2str(t) , ' Swarm.GBEST.O = ' , num2str(Swarm.GBEST.O)];
+        disp(outmsg);
+    end
+    
+    cgCurve(t) = Swarm.GBEST.O;
+    average_objective(t) = average_objective(t) / noP;
+    
+    fileName = ['Resluts after iteration # ' , num2str(t)];
+    save( fileName)
+end
+GBEST = Swarm.GBEST;
+
+
+    
+    
 
 
 
--- main.m
+++ main.m
@@ -13,11 +13,10 @@ clc
 
 % Defintion:
 % Parameters of problem:
-problem.d = 2; %Number of dimensions
-problem.lb = 50 * ones(1, 2);%Lower bound
-problem.ub = -50 * ones(1, 2);%Upper bound
-problem.fobj = @ObjectiveFunction; %Objective function
-
+problem.nVar = 2;
+problem.ub = 50 * ones(1, 2);
+problem.lb = -50 * ones(1, 2);
+problem.fobj = @ObjectiveFunction;
 %Hyperparameters of PSO the algorithm;
 
 pso.wMax = 0.9; %Inertia
@@ -25,9 +24,9 @@ pso.wMin = 0.2;%Inertia
 pso.c1 = 2;%Cognition of particle 
 pso.c2 = 2;%Social influence of swarm  
 pso.vMax = (problem.ub - problem.lb).*0.2;
-pso.vmin = -vMax;
+pso.vmin = -pso.vMax;
 
-Nop = 4; %Number of particles (N)
+noP = 4; %Number of particles (N)
 max_iter = 500;  % Maximum number of iterations (max_iter)
 
 visFlag = 1; % set this to 0 if you do not want visualization
@@ -35,7 +34,7 @@ visFlag = 1; % set this to 0 if you do not want visualization
 RunNo  = 30; 
 BestSolutions_PSO = zeros(1 , RunNo);
 
-[ GBEST  , cgcurve ] = PSO( noP , maxIter, problem ,pso, visFlag ) ;
+[ GBEST  , cgcurve ] = PSO( noP , max_iter ,pso, problem, visFlag ) ;
 
  disp('Best solution found')
  GBEST.X
